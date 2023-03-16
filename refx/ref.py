137343cd6edb3a32ddece86e3d83395a8fc110ed
Cristian Garcia <cgarcia.e88@gmail.com>
2023-03-16 06:18:30 +0000

    ValueIndex

--- refx/ref.py
+++ refx/ref.py
@@ -5,15 +5,13 @@ import threading
 import typing as tp
 
 import jax
+import simple_pytree as spt
 
-from refx import ids, tracers
+from refx import tracers
 
 A = tp.TypeVar("A")
 B = tp.TypeVar("B")
-C = tp.TypeVar("C")
 F = tp.TypeVar("F", bound=tp.Callable[..., tp.Any])
-G = tp.TypeVar("G", bound=tp.Callable[..., tp.Any])
-H = tp.TypeVar("H", bound=tp.Callable[..., tp.Any])
 
 
 class Nothing:
@@ -33,10 +31,6 @@ class _Context(threading.local):
     ref_context_stack: tp.List[_RefContext] = dataclasses.field(
         default_factory=lambda: [_RefContext(0)]
     )
-    barrier_pytree: tp.Any = NOTHING
-    # is_crossing_barrier: bool = False
-    # unflattening_ref_cache: tp.Optional[tp.Dict["Ref[tp.Any]", "Ref[tp.Any]"]] = None
-    # flattening_value_cache: tp.Optional[tp.Dict["Ref[tp.Any]", tp.Any]] = None
 
     @property
     def current_ref_context(self) -> _RefContext:
@@ -58,15 +52,10 @@ def incremented_ref():
 
 
 class Ref(tp.Generic[A]):
-    def __init__(self, value: A, *, id: tp.Optional[ids.Id] = None):
+    def __init__(self, value: A):
         self._value = value
-        self._id = ids.uuid() if id is None else id
         self._ref_context = _CONTEXT.current_ref_context
         self._trace_level = tracers.current_trace_level()
-
-    @property
-    def id(self) -> ids.Id:
-        return self._id
 
     @property
     def value(self) -> A:
@@ -88,6 +77,12 @@ class Ref(tp.Generic[A]):
         return Ref(self.value)
 
 
+@dataclasses.dataclass
+class ValueIndex(spt.Pytree):
+    value: tp.Any
+    index: int = spt.static_field()
+
+
 def clone_references(pytree: tp.Any) -> tp.Any:
     cache: tp.Dict[Ref[tp.Any], Ref[tp.Any]] = {}
 
@@ -101,60 +96,31 @@ def clone_references(pytree: tp.Any) -> tp.Any:
     return jax.tree_map(clone_fn, pytree)
 
 
-# @contextlib.contextmanager
-# def crossing_barrier():
-#     _CONTEXT.unflattening_ref_cache = {}
-#     _CONTEXT.flattening_value_cache = {}
-#     _CONTEXT.is_crossing_barrier = True
-#     try:
-#         yield
-#     finally:
-#         _CONTEXT.unflattening_ref_cache = None
-#         _CONTEXT.flattening_value_cache = None
-#         _CONTEXT.is_crossing_barrier = False
+def deref(pytree: A) -> A:
+    ref_index: tp.Dict[Ref[tp.Any], int] = {}
 
-
-# def _update_ref_context(pytree: tp.Any) -> tp.Any:
-#     for pytree in jax.tree_util.tree_leaves(
-#         pytree, is_leaf=lambda x: isinstance(x, PytreeRef)
-#     ):
-#         if isinstance(pytree, PytreeRef):
-#             pytree.ref._trace_level = tracers.current_trace_level()
-#             pytree.ref._ref_context = _CONTEXT.current_ref_context
-
-
-def deref(
-    pytree: A, callback: tp.Optional[tp.Callable[[Ref[tp.Any]], None]] = None
-) -> A:
     def deref_fn(ref: tp.Any) -> tp.Any:
         if isinstance(ref, Ref):
-            if callback is not None:
-                callback(ref)
-            return ref.value
+            if ref not in ref_index:
+                ref_index[ref] = len(ref_index)
+            return ValueIndex(ref.value, index=ref_index[ref])
         return ref
 
     return jax.tree_map(deref_fn, pytree)
 
 
-def reref(
-    values: A,
-    refs: A,
-    callback: tp.Optional[tp.Callable[[Ref[tp.Any]], Ref[tp.Any]]] = None,
-) -> A:
-    ref_cache: tp.Dict[Ref[tp.Any], Ref[tp.Any]] = {}
+def reref(pytree: A) -> A:
+    index_ref: tp.Dict[int, Ref[tp.Any]] = {}
 
-    def reref_fn(value: tp.Any, ref: tp.Any) -> tp.Any:
-        if isinstance(ref, Ref):
-            assert not isinstance(value, Ref)
-            if ref not in ref_cache:
-                ref_cache[ref] = Ref(value, id=ref.id)
-            ref = ref_cache[ref]
-            if callback is not None:
-                ref = callback(ref)
+    def reref_fn(value_index: tp.Any) -> tp.Any:
+        if isinstance(value_index, ValueIndex):
+            if value_index.index not in index_ref:
+                index_ref[value_index.index] = Ref(value_index.value)
+            ref = index_ref[value_index.index]
             return ref
-        return value
+        return value_index
 
-    return jax.tree_map(reref_fn, values, refs)
+    return jax.tree_map(reref_fn, pytree, is_leaf=lambda x: isinstance(x, ValueIndex))
 
 
 def cross_barrier(decorator: F, *decorator_args, **decorator_kwargs) -> F:
@@ -169,13 +135,10 @@ def cross_barrier(decorator: F, *decorator_args, **decorator_kwargs) -> F:
         @functools.wraps(f)
         def inner_wrapper(*args, **kwargs):
             with incremented_ref():
-                args, kwargs = reref((args, kwargs), _CONTEXT.barrier_pytree)
-                _CONTEXT.barrier_pytree = NOTHING
+                # rereference inputs
+                args, kwargs = reref((args, kwargs))
                 out = f(*args, **kwargs)
-
-            # add the refs from the output to the cache
-            _CONTEXT.barrier_pytree = out
-            # dereference the output so it can be returned
+            # dereference the output so it can be returned through the barrier
             out = deref(out)
             return out
 
@@ -183,141 +146,13 @@ def cross_barrier(decorator: F, *decorator_args, **decorator_kwargs) -> F:
 
         @functools.wraps(f)
         def outer_wrapper(*args, **kwargs) -> B:
-            # gather all input references and their values
-            input_id_ref: tp.Dict[ids.Id, Ref[tp.Any]] = {}
-
-            def add_refs(ref: Ref[tp.Any]):
-                input_id_ref[ref.id] = ref
-
-            # put original input references in the context
-            _CONTEXT.barrier_pytree = (args, kwargs)
             # dereference inputs so they can pass through the barrier
-            args, kwargs = deref((args, kwargs), callback=add_refs)
-            try:
-                out = barrier(*args, **kwargs)
-
-                def update_inputs_refs(output_ref: Ref[A]) -> Ref[A]:
-                    # if the output reference has the same id as an input reference
-                    # update the input reference with the value from the output
-                    # reference, and return the input reference to form part of
-                    # the 'out' pytree
-                    if output_ref.id in input_id_ref:
-                        input_ref = input_id_ref[output_ref.id]
-                        input_ref.value = output_ref.value
-                        return input_ref
-                    # otherwise, return the output reference
-                    return output_ref
-
-                out = reref(out, _CONTEXT.barrier_pytree, callback=update_inputs_refs)
-            finally:
-                _CONTEXT.barrier_pytree = NOTHING
-
+            args, kwargs = deref((args, kwargs))
+            out = barrier(*args, **kwargs)
+            # rereference the output
+            out = reref(out)
             return out
 
         return outer_wrapper
 
     return decorator_wrapper
-
-
-# class Base(tp.Generic[A]):
-#     @abstractmethod
-#     def __init__(self, value: A):
-#         ...
-
-#     @property
-#     @abstractmethod
-#     def value(self) -> A:
-#         ...
-
-#     @value.setter
-#     @abstractmethod
-#     def value(self, value: A):
-#         ...
-
-
-# class PytreeValue(Base[A]):
-#     def __init__(self, value: A):
-#         self._value = value
-
-#     @property
-#     def value(self) -> A:
-#         return self._value
-
-#     @value.setter
-#     def value(self, value: A):
-#         raise AttributeError(f"Cannot mutate PytreeValue '{type(self).__name__}'")
-
-
-# def flatten_pytree_value(pytree: PytreeValue[A]) -> tp.Tuple[tp.Tuple[A], None]:
-#     return (pytree.value,), None
-
-
-# def unflatten_pytree_value(_, children: tp.Tuple[A]) -> PytreeValue[A]:
-#     return PytreeValue(children[0])
-
-
-# class PytreeRef(Base[A]):
-#     def __init__(self, ref_or_value: tp.Union[Ref[A], A]):
-#         if isinstance(ref_or_value, Ref):
-#             self._ref = ref_or_value
-#         else:
-#             self._ref = Ref(ref_or_value)
-
-#     @property
-#     def id(self) -> ids.Id:
-#         return self.ref.id
-
-#     @property
-#     def ref(self) -> Ref[A]:
-#         return self._ref
-
-#     @property
-#     def value(self) -> A:
-#         return self.ref.value
-
-#     @value.setter
-#     def value(self, value: A):
-#         self.ref.value = value
-
-
-# class _Missing:
-#     pass
-
-
-# MISSING = _Missing()
-
-
-# def flatten_pytree_ref(
-#     pytree: PytreeRef[A],
-# ) -> tp.Tuple[tp.Tuple[tp.Union[A, Ref[A]]], tp.Optional[Ref[A]]]:
-#     if _CONTEXT.is_crossing_barrier:
-#         assert _CONTEXT.flattening_value_cache is not None
-#         ref = pytree.ref
-#         if ref.value is not MISSING:
-#             _CONTEXT.flattening_value_cache[ref] = ref.value
-#         value = _CONTEXT.flattening_value_cache[ref]
-#         ref._value = MISSING
-#         return (value,), ref
-#     else:
-#         return (pytree.ref,), None
-
-
-# def unflatten_pytree_ref(
-#     ref: tp.Optional[Ref[A]], children: tp.Tuple[tp.Union[A, Ref[A]]]
-# ) -> PytreeRef[A]:
-#     value = children[0]
-
-#     if _CONTEXT.is_crossing_barrier:
-#         assert _CONTEXT.unflattening_ref_cache is not None
-#         assert not isinstance(value, Ref)
-#         assert ref is not None
-#         if ref not in _CONTEXT.unflattening_ref_cache:
-#             _CONTEXT.unflattening_ref_cache[ref] = Ref(value, id=ref.id)
-#         new_ref = _CONTEXT.unflattening_ref_cache[ref]
-#         return PytreeRef(new_ref)
-#     else:
-#         assert isinstance(value, Ref)
-#         return PytreeRef(value)
-
-
-# jax.tree_util.register_pytree_node(PytreeRef, flatten_pytree_ref, unflatten_pytree_ref)
--- tests/test_ref.py
+++ tests/test_ref.py
@@ -75,32 +75,33 @@ class TestPytreeRef:
 
             r2.value = 2
             assert r1 is not r2
-            assert r1.id is r2.id
+            assert r3.value == 2
             return r2
 
         r2 = g(r1, r1)
 
-        assert r1.value == 2
+        assert r1.value == 1
         assert r2.value == 2
 
         r2.value = 3
-        assert r1.value == 3
+        assert r1.value == 1
         assert r2.value == 3
 
         r3 = g(r1, r1)
 
-        assert r3 is r2
+        assert r3 is not r2
         assert r3.value == 2
 
         @jax.jit
         def h():
-            p4 = refx.clone_references(r3)
+            r4 = refx.clone_references(r3)
             with jax.ensure_compile_time_eval():
-                assert p4.value == 2
-            assert p4.ref is not r3.ref
-            assert p4.id is not r3.id
-            p4.value = 5
-            assert p4.value == 5
+                assert r4.value == 2
+            assert r4 is not r3
+            r4.value = 5
+            assert r4.value == 5
+            with jax.ensure_compile_time_eval():
+                assert r3.value == 2
 
         h()
 
@@ -110,18 +111,20 @@ class TestPytreeRef:
         @refx.cross_barrier(jax.jit)
         def g(r2: refx.Ref[int]):
             r2.value += 1
-            assert r1.ref is not r2.ref
+            assert r1 is not r2
             return r2
 
         r2 = g(r1)
-        assert r1.ref is r2.ref
-        assert r1.value == 2
+        assert r1 is not r2
+        assert r1.value == 1
         assert r2.value == 2
 
-        r2 = g(r1)
-        assert r1.ref is r2.ref
-        assert r1.value == 3
-        assert r2.value == 3
+        r3 = g(r2)
+        assert r1 is not r2
+        assert r2 is not r3
+        assert r1.value == 1
+        assert r2.value == 2
+        assert r3.value == 3
 
         # test passing a reference to a jitted function without refx.cross_barrier
         @jax.jit
@@ -131,8 +134,37 @@ class TestPytreeRef:
         with pytest.raises(TypeError, match="Cannot interpret value of type"):
             f(r1)
 
-        assert isinstance(r1.value, jax.Array)
-        assert r1.value == 3
+        assert isinstance(r1.value, int)
+        assert r1.value == 1
+
+    def test_no_rejit(self):
+        n = 0
+        r1 = refx.Ref(1)
+        r2 = refx.Ref(2)
+
+        @refx.cross_barrier(jax.jit)
+        def g(r3: refx.Ref[int], r4: refx.Ref[int], r5: refx.Ref[int]):
+            nonlocal n
+            n += 1
+            assert r3 is r4
+            assert r4 is not r5
+            return r3
+
+        r6 = g(r1, r1, r2)
+        assert r6 is not r1
+        assert r6.value == r1.value
+        assert n == 1
+
+        g(r1, r1, r2)
+        assert n == 1
+
+        g(r2, r2, r1)
+        assert n == 1
+
+        with pytest.raises(AssertionError):
+            g(r1, r2, r1)
+
+        assert n == 2
 
 
 class TestRefField:
@@ -173,3 +205,19 @@ class TestRefField:
 
         assert foo1.a == 3
         assert foo2.a == 3
+
+    def test_barrier(self):
+        @dataclasses.dataclass
+        class Foo(Pytree):
+            a: int = refx.field(type=refx.Ref[int])
+
+        foo1 = Foo(a=1)
+
+        @refx.cross_barrier(jax.jit)
+        def g(foo2: Foo):
+            foo2.a += 1
+            return foo2
+
+        foo2 = g(foo1)
+        assert foo1.a == 1
+        assert foo2.a == 2
